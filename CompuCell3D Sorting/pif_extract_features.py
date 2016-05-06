54529ab4c72ecd094ee549830baf14e5b6842418
Dhananjay Bhaskar <dhananjay.bhaskar.92@gmail.com>
2016-05-06 00:47:42 -0700

    Fixed bugs

--- CompuCell3D Sorting/pif_extract_features.py
+++ CompuCell3D Sorting/pif_extract_features.py
@@ -6,18 +6,19 @@
 # Extract cell shape features from PIF file
 # 
 
+import math
+import string
 import numpy as NP
 from optparse import OptionParser
 
 pifFile = ''
-height = -1
-width = -1
-
+l_height = -1
+l_width = -1
 
 parser = OptionParser()
 
 parser.add_option("-i", "--input", action="store", type="string", dest="inputfile", help="path to PIF file", metavar="PIF")
-parser.add_option("-h", "--height", action="store", type="int", dest="height", help="lattice HEIGHT", metavar="HEIGHT")
+parser.add_option("-l", "--height", action="store", type="int", dest="height", help="lattice HEIGHT", metavar="HEIGHT")
 parser.add_option("-w", "--width", action="store", type="int", dest="width", help="lattice WIDTH", metavar="WIDTH")
 
 (options, args) = parser.parse_args()
@@ -25,11 +26,11 @@ parser.add_option("-w", "--width", action="store", type="int", dest="width", hel
 if options.inputfile:
 	pifFile = options.inputfile
 if options.height:
-	height = options.height
+	l_height = options.height
 if options.width:
-	width = options.width
+	l_width = options.width
 	
-lattice = open(pifFile).read().split('\n')
+lattice = open(pifFile).read().split('\n')[1:-1]
 
 cellDict = dict()
 
@@ -39,15 +40,15 @@ for pix in lattice:
 
 	cell_id = int(fields[0])
 	pix_x = int(fields[3])
-    pix_y = int(fields[5])
+	pix_y = int(fields[5])
             
 	if cell_id not in cellDict.keys():
     
-    	cellDict[cell_id] = [[pix_x,pix_y]]
+		cellDict[cell_id] = [[pix_x,pix_y]]
 
 	cellDict[cell_id].append([pix_x,pix_y])
-
-
+	
+	
 # Extract features from a list of pixels representing a cell
 
 class ExtractFeatures:
@@ -58,4 +59,16 @@ class ExtractFeatures:
 		    
 	def area(self):
 	
-		return len(cell_pixel_list)
+		return len(self.pix_list)
+	
+# Compute feature vector of each cell for further classification
+
+featureDict = dict()
+
+for cell_id in cellDict.keys():
+
+	extractor = ExtractFeatures(cellDict[cell_id])
+
+	featureDict[cell_id] = [extractor.area()]
+	
+print featureDict[1]
