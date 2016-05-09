7bcdeadc245168df2d129adf15ee003086efd49d
Darrick Lee <y.l.darrick@gmail.com>
2016-05-08 21:46:48 -0700

    Added ellipse properties to feature extraction

--- CompuCell3D Sorting/pif_extract_features.py
+++ CompuCell3D Sorting/pif_extract_features.py
@@ -9,6 +9,7 @@
 import math
 import string
 import numpy as NP
+import skimage.measure 
 from optparse import OptionParser
 
 pifFile = ''
@@ -47,7 +48,13 @@ for pix in lattice:
 		cellDict[cell_id] = [[pix_x,pix_y]]
 
 	cellDict[cell_id].append([pix_x,pix_y])
-	
+
+# Find the maximum x and y pixel location (used to create labeled img for regionprops)
+all_xpix = [int(pix.split()[3]) for pix in lattice]
+all_ypix = [int(pix.split()[5]) for pix in lattice]
+
+xres = max(all_xpix)
+yres = max(all_ypix)
 	
 # Extract features from a list of pixels representing a cell
 
@@ -59,9 +66,50 @@ class ExtractFeatures:
 		    
 	def area(self):
 	
-		return len(self.pix_list)
+		return len(self.pix_list) 
+
+	def ellipse_props(self):
+		# Returns list of properties derived from fitting ellipse (in the following order)
+		# centroid_x, centroid_y, eccentricity, eulerNumber, extent, majorAxisLength,
+		# minorAxisLength, orientation, perimeter, solidity
+
+		# Start by creating labeled_img 
+		labeled_img = NP.zeros([x_res, y_res])
+		
+		for (x_pix, y_pix) in self.pix_list:
+			labeled_img[x_pix-1, y_pix-1] = 1
+
+		props = skimage.measure.regionprops(NP.int_(labeled_img))
+
+		centroid = props[0].centroid
+
+		ellipse_prop_list = [centroid[0]]
+		ellipse_prop_list.append(centroid[1])
+		ellipse_prop_list.append(props[0].eccentricity)
+		ellipse_prop_list.append(props[0].euler_number)
+		ellipse_prop_list.append(props[0].extent)
+		ellipse_prop_list.append(props[0].major_axis_length)
+		ellipse_prop_list.append(props[0].minor_axis_length)
+		ellipse_prop_list.append(props[0].orientation)
+		ellipse_prop_list.append(props[0].perimeter)
+		ellipse_prop_list.append(props[0].solidity)
+
+		return ellipse_prop_list
+
 	
 # Compute feature vector of each cell for further classification
+# Order of features in the feature vector:
+# 	area
+# 	centroid_x
+# 	centroid_y
+# 	eccentricity
+# 	eulerNumber
+# 	extent
+# 	majorAxisLength
+# 	minorAxisLength
+# 	orientation
+# 	perimeter
+# 	solidity
 
 featureDict = dict()
 
@@ -70,5 +118,6 @@ for cell_id in cellDict.keys():
 	extractor = ExtractFeatures(cellDict[cell_id])
 
 	featureDict[cell_id] = [extractor.area()]
+	featureDict[cell_id] = featureDict[cell_id] + extractor.ellipse_props()
 	
 print featureDict[1]
