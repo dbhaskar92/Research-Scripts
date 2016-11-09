e16590bccfc72513adb4a08a34ee0b3ab061a5a3
Dhananjay Bhaskar <dhananjay.bhaskar.92@gmail.com>
2016-11-08 20:23:48 -0800

    Added MAT to PIF conversion

---
+++ Image Segmentation/image_segment.m
@@ -0,0 +1,237 @@
+%{ 
+%   Author: Dhananjay Bhaskar <dbhaskar92@math.ubc.ca>
+%   Last modified: Nov 06, 2016
+%   Description: Segment cancer cell line images (Phase contrast microscopy)
+%   Tested on MATLAB R2011a
+%}
+
+%% Main
+function [] = image_segment()
+    
+	nfigs = 1;
+
+	test_1(nfigs, 1);
+
+	function [nfigs] = test_1(nfigs, display)
+		MIAPaCa_6 = 'MIAPaCa_6.JPG';
+		% edge detection using mathematical morphology
+		[mm_fg, mm_outline, nfigs] = morphological_segment(MIAPaCa_6, 1, 6, 1000, nfigs, display);
+		% compute foreground markers
+		foreground_markers = zeros(size(mm_fg,1), size(mm_fg,2));
+		foreground_markers(mm_fg > 0) = 1;
+		sedisk = strel('disk', 4);
+		for i = 1 : 10
+			foreground_markers = imerode(foreground_markers, sedisk);
+		end
+		foreground_markers = bwareaopen(foreground_markers, 10);
+		% marker-based watershed
+		[w_cells, w_borders, nfigs] = watershed_segment(MIAPaCa_6, foreground_markers, nfigs, display);
+		% opening by reconstruction and closing by reconstruction watershed
+		[wobrcbr_cells, wobrcbr_borders, nfigs] = watershed_obrcbr_segment(MIAPaCa_6, 15, 10, 1000, nfigs, display);
+		% plot results
+		[nfigs] = plot_results(mm_fg, mm_outline, w_cells, w_borders, wobrcbr_cells, wobrcbr_borders, 'MIAPaCa_6', nfigs);
+		% save correct segmentations
+		save_segmentation('MIAPaCa_6_segmented.mat', [2 4 6 12 15 21 23 25 35 38 39 40 42], w_cells);
+		save_segmentation('MIAPaCa_6_segmented.mat', [16 19 20], wobrcbr_cells);
+		save_PIF('MIAPaCa_6_segmented.mat', 'MIAPaCa_6.pif');
+	end
+    
+	function [nfigs] = test_2(nfigs, display)
+		MIAPaCa_3 = 'MIAPaCa_3.tif';
+		% edge detection using mathematical morphology
+		[mm_fg, mm_outline, nfigs] = morphological_segment(MIAPaCa_3, 1, 6, 1000, nfigs, display);
+		% compute foreground markers
+		foreground_markers = zeros(size(mm_fg,1), size(mm_fg,2));
+		foreground_markers(mm_fg > 0) = 1;
+		sedisk = strel('disk', 4);
+		for i = 1 : 6
+			foreground_markers = imerode(foreground_markers, sedisk);
+		end
+		foreground_markers = bwareaopen(foreground_markers, 10);
+		% marker-based watershed
+		[w_cells, w_borders, nfigs] = watershed_segment(MIAPaCa_3, foreground_markers, nfigs, display);
+		% opening by reconstruction and closing by reconstruction watershed
+		[wobrcbr_cells, wobrcbr_borders, nfigs] = watershed_obrcbr_segment(MIAPaCa_3, 15, 10, 1000, nfigs, display);
+		% plot results
+		[nfigs] = plot_results(mm_fg, mm_outline, w_cells, w_borders, wobrcbr_cells, wobrcbr_borders, 'MIAPaCa_3', nfigs);
+		% save correct segmentations
+		save_segmentation('MIAPaCa_3_segmented.mat', [10 12 17 26], mm_fg);
+		save_segmentation('MIAPaCa_3_segmented.mat', [20 33], w_cells);
+		save_PIF('MIAPaCa_3_segmented.mat', 'MIAPaCa_3.pif');
+	end
+
+	function [nfigs] = test_3(nfigs, display)
+		MIAPaCa_32 = strcat('dataset',filesep,'MIAPaCa_32.tif');
+		% edge detection using mathematical morphology
+		[mm_fg, mm_outline, nfigs] = morphological_segment(MIAPaCa_32, 0.9, 8, 1000, nfigs, display);
+		% compute foreground markers
+		foreground_markers = zeros(size(mm_fg,1), size(mm_fg,2));
+		foreground_markers(mm_fg > 0) = 1;
+		sedisk = strel('disk', 4);
+		for i = 1 : 9
+			foreground_markers = imerode(foreground_markers, sedisk);
+		end
+		foreground_markers = bwareaopen(foreground_markers, 10);
+		% marker-based watershed
+		[w_cells, w_borders, nfigs] = watershed_segment(MIAPaCa_32, foreground_markers, nfigs, display);
+		% opening by reconstruction and closing by reconstruction watershed
+		[wobrcbr_cells, wobrcbr_borders, nfigs] = watershed_obrcbr_segment(MIAPaCa_32, 15, 20, 1000, nfigs, display);
+		% plot results
+		[nfigs] = plot_results(mm_fg, mm_outline, w_cells, w_borders, wobrcbr_cells, wobrcbr_borders, 'MIAPaCa_32', nfigs);
+		% save correct segmentations
+		save_segmentation('MIAPaCa_32_segmented.mat', [6 7 12 11 13 15 17 18 21], mm_fg);
+		save_segmentation('MIAPaCa_32_segmented.mat', [12 13 18 21 22 24], w_cells);
+		save_PIF('MIAPaCa_32_segmented.mat', 'MIAPaCa_32.pif');
+	end
+    
+end
+
+%% Plot results
+function [fig_cnt] = plot_results(mm_fg, mm_outline, w_cells, w_borders, wobrcbr_cells, wobrcbr_borders, fname, fig_cnt)
+
+	mm_fg_color = label2rgb(mm_fg, 'jet', [.7 .7 .7], 'shuffle');
+	mm_outline_color = label2rgb(imdilate(mm_outline, ones(3,3)), 'jet', [.7 .7 .7], 'shuffle');
+	w_cells_color = label2rgb(w_cells, 'jet', [.7 .7 .7], 'shuffle');
+	w_borders_color = label2rgb(imdilate(w_borders, ones(3,3)), 'jet', [.7 .7 .7], 'shuffle');
+	wobrcbr_cells_color = label2rgb(wobrcbr_cells, 'jet', [.7 .7 .7], 'shuffle');
+	wobrcbr_borders_color = label2rgb(imdilate(wobrcbr_borders, ones(3,3)), 'jet', [.7 .7 .7], 'shuffle');
+
+	figure(fig_cnt)
+
+	subplot(2,3,1), imshow(mm_fg_color), title('MM Foreground')
+	subplot(2,3,2), imshow(w_cells_color), title('Watershed (Marker) Foreground')
+	subplot(2,3,3), imshow(wobrcbr_cells_color), title('Watershed (OBRCBR) Foreground')
+
+	mm_plot = subplot(2,3,4); imshow(mm_outline_color), title('MM Outline')
+	classes = numel(unique(reshape(mm_fg, [1, numel(mm_fg)])));
+	for i = 1 : classes-1
+		tmp = zeros(size(mm_fg,1), size(mm_fg,2));
+		tmp(mm_fg == i) = 1;
+		struct_array = regionprops(tmp, 'centroid');
+		if ~isempty(struct_array) && isfield(struct_array, 'Centroid')
+			if ~isempty(struct_array.Centroid) && numel(extractfield(struct_array, 'Centroid')) == 2
+				centroid = cat(1, struct_array.Centroid);
+				txt = text(centroid(1), centroid(2), int2str(i));
+				set(txt, 'fontsize', 5);
+			end
+		end
+	end
+
+	wm_plot = subplot(2,3,5); imshow(w_borders_color), title('Watershed (Marker) Outlines')
+	classes = numel(unique(reshape(w_cells, [1, numel(w_cells)])));
+	for i = 1 : classes-1
+		tmp = zeros(size(w_cells,1), size(w_cells,2));
+		tmp(w_cells == i) = 1;
+		struct_array = regionprops(tmp, 'centroid');
+		if ~isempty(struct_array) && isfield(struct_array, 'Centroid')
+			if ~isempty(struct_array.Centroid) && numel(extractfield(struct_array, 'Centroid')) == 2
+				centroid = cat(1, struct_array.Centroid);
+				txt = text(centroid(1), centroid(2), int2str(i));
+				set(txt, 'fontsize', 5);
+			end
+		end
+	end
+
+	wobrcbr_plot = subplot(2,3,6); imshow(wobrcbr_borders_color), title('Watershed (OBRCBR) Outlines')
+	classes = numel(unique(reshape(wobrcbr_cells, [1, numel(wobrcbr_cells)])));
+	for i = 1 : classes-1
+		tmp = zeros(size(wobrcbr_cells,1), size(wobrcbr_cells,2));
+		tmp(wobrcbr_cells == i) = 1;
+		struct_array = regionprops(tmp, 'centroid');
+		if ~isempty(struct_array) && isfield(struct_array, 'Centroid')
+			if ~isempty(struct_array.Centroid) && numel(extractfield(struct_array, 'Centroid')) == 2
+				centroid = cat(1, struct_array.Centroid);
+				txt = text(centroid(1), centroid(2), int2str(i));
+				set(txt, 'fontsize', 5);
+			end
+		end
+	end
+
+	% Print outlines
+
+	fig = figure;
+	set(mm_plot, 'Visible', 'off');
+	mm_fig = copyobj(mm_plot, fig);
+	set(mm_fig, 'Position', get(0, 'DefaultAxesPosition'));
+	saveas(mm_fig, strcat(fname, '_MM', '.png'));
+
+	fig = figure;
+	set(wm_plot, 'Visible', 'off');
+	wm_fig = copyobj(wm_plot, fig);
+	set(wm_fig, 'Position', get(0, 'DefaultAxesPosition'));
+	saveas(wm_fig, strcat(fname, '_WS', '.png'));
+
+	fig = figure;
+	set(wobrcbr_plot, 'Visible', 'off');
+	wobrcbr_fig = copyobj(wobrcbr_plot, fig);
+	set(wobrcbr_fig, 'Position', get(0, 'DefaultAxesPosition'));
+	saveas(wobrcbr_fig, strcat(fname, '_WOBRCBR', '.png'));
+
+	fig_cnt = fig_cnt + 1;
+    
+end
+
+%% Save segmentation
+function save_segmentation(filename, cell_labels, labelled_image)
+
+	segmentations = zeros(size(labelled_image,1), size(labelled_image,2));
+	max_cid = 0;
+
+	if exist(filename, 'file')
+		load(filename, 'segmentations', 'max_cid');
+		assert(isequal(size(segmentations),size(labelled_image)), 'Matrix dimensions do not match.');  
+	end
+
+	num_correct_segmentations = numel(cell_labels);
+
+	for i = 1 : num_correct_segmentations
+		max_cid = max_cid + 1;
+		class = cell_labels(i);
+		segmentations(labelled_image == class) = max_cid;
+	end
+
+	save(filename, 'segmentations', 'max_cid');
+    
+end
+
+%%  Convert MAT to PIF file
+function save_PIF(filename, PIFname)
+
+	segmentations = [];
+
+	if exist(filename, 'file')
+		load(filename, 'segmentations');
+	else
+		print('Error: MAT file does not exist.'); 
+	end
+
+	cell_ids = unique(nonzeros(segmentations));
+	numPIFrows = nnz(segmentations);
+
+	PIF_data = zeros(numPIFrows, 5);
+	ind = 1;
+	for i = 1 : length(cell_ids)
+		[rows, cols] = find(segmentations == cell_ids(i));
+		for cnt = 1 : length(rows)
+			PIF_data(ind, 1) = cell_ids(i);
+			PIF_data(ind, 2) = rows(cnt);
+			PIF_data(ind, 3) = rows(cnt);
+			PIF_data(ind, 4) = cols(cnt);
+			PIF_data(ind, 5) = cols(cnt);
+			ind = ind + 1;
+		end
+	end
+    
+    if exist(PIFname, 'file')
+		print('Error: PIF file already exists.');
+    else
+		fileID = fopen(PIFname, 'w');
+		cnt = 1;
+		while cnt < ind
+			fprintf(fileID, '%d CellU %d %d %d %d\n', PIF_data(cnt,:));
+			cnt = cnt + 1;
+		end
+		fclose(fileID);
+    end
+
+end
